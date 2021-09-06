57b1e9e5aa92af12afff8a8e445ca716f94a1273
Galen Rhodes <galenrhodes@me.com>
2021-09-06 07:08:51 -0400

    Documentation...

--- Tests/RedBlackTreeTests/NodeDrawing.swift
+++ Tests/RedBlackTreeTests/NodeDrawing.swift
@@ -48,7 +48,7 @@ extension TreeNode where T == BinaryTreeDictionary<String, NodeTestValue>.KV {
     }
 
     private func drawLine(to child: TreeNode<T>) {
-        let angle:         CGFloat      = (theta * (child === self.leftNode ? leftAngle : rightAngle))
+        let angle:         CGFloat      = (theta * (child === leftNode ? leftAngle : rightAngle))
         let parentLoc:     NSPoint      = value.value.bounds.origin
         let childLoc:      NSPoint      = child.value.value.bounds.origin
         let startPoint:    NSPoint      = NSPoint(x: parentLoc.x + (nodeRadius * cos(angle)), y: parentLoc.y - (nodeRadius * sin(angle)))
